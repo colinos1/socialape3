e594c648761ad8cea13455ce34637fb89dfa1527
colinos1 <67876558+colinos1@users.noreply.github.com>
2020-07-05 16:17:52 -0400

    Add files via upload

---
+++ sc/function/Functions/util/config.js
@@ -0,0 +1,27 @@
+// module.exports = {
+//   "type": "service_account",
+//   "project_id": "socialape-4e17c",
+//   "private_key_id": "a95409eb285ab1ed150ac350e018cdcad880a3da",
+//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC+0ox0acxxCyvF\nKIbxgbKylh3UOJDi+PtZT8FJNKkWrKwmgSlFl9w8cxUFdukdpFy2di2xx6uar4d1\nhbOomkhLHOvWPqw+F0UtMLedGxAYa7yJ/pKxg88e56I8YiHpDnT+0pIf6Ll68Ef9\njJpaSm639Igpue3QH9TU1qiUujGxonhkihqSQPVWjeBKXA4qdtlxaOFL3skWZCFt\ntjWg/OILZW8xhbdvImcvTIz4Tq2YlHjCPEeB3o8jqq2T4Sj/ki3KMxqw+0ptp+kV\nLuDecGLt3s9ZKdunyuN2N22wm7mqr8+H8UijLOmCdnAbRUrb0wY7BeScbTnwuUdC\ntZ3d690lAgMBAAECggEAVBDd38tcTdDmD42zJWJGH3cS3ROugM+/q7D/LgkNFPgt\nPylXzVkBpX/tmuKwAGcQkTGvH8sfRp5dxuCILZcKkkzDeTdTGmeWCjqUKZpqlNeh\n0AlfBuVSsgLJ7mAtIEdGM50A0x3r2MVScdknhu+Tp2cLNZa7thzP3pRS4HLGwcJW\n1xpMF+8F4yiv29CMFwa9+HWO1usj0LwfrkW2XQo1DXKFz6SpoBWu5MbF8IvOfJPg\nUTW8KaYaAsK0531ErfPIPiExuEEIr6m8oDpx1F/VP6I+8lENj6MQr5DTq2CExNy7\nCqU7UZ8oeGYsWYJPy0OHVTc76ZgkPVXGacFyMhB1YQKBgQDoun8GS+IwSPPCKnvg\npwXlNg4SwaSQqFxBxgCLPCi1DcefHggs21seN7UKIU2g7rI9qtE+2sFjx65WHfLu\nQ9k7D6qBaYLqp8a5G+h6OwfMbdoiEBXGO6uj3zzC9RaaFnFKJXGwPGmxj93VDxqD\nwZYtd9RsGTul3T8f6SWUhVLYDwKBgQDR51Hp6Fr1aR6GNyNsgaSMrPWXLAKDX1kW\n0gJf9b3QJDVMZ2IUJY2yI+ANYSrosl/z+IIcTpGKDFsu3fiepoJYXBulUziGfyMe\nJuTHHmSZ6NzlSF5+ikNr+JIU9DraucFAufUUZ2EHFASYfymhmZrz7GshQa+TdcYF\nBaY0KLGjiwKBgQCD4mPnmoskl8LjTpYC2kPh/7777BapD2+wqldWkBH1gEiWqDsz\nyF8bgbArfHH0eDAz+CA6AVfEhr0J+lDXq0VoTdlvGu7gsZFRBzq4QcmKlNofoYPl\nYZ1D8VcKmD9ltMAgp5WErMesov2wJq386nCDdbvAQpBlzzpdiA8O2nr3CwKBgQCi\nuDVbJEJnHw+HsnTmp/XdDDPy9i4Yk7DnrPQkWcfE9FzRFxAEWDxpTcBh/uKZs7YI\n1qxer1XJ89w4D+Gtc512TCkuV75pHWQm+rW92sTYP5oGsLx4Uk0EBZWRYufBKeEz\ngTUJlT61kwIhCetHGGH0GGHQTTCEGvfgasm2g5l6EwKBgQCAIsxY9oYU+AESYFrk\nrQ+CHsPr18UAfcPkTjisjAsCGro+vXKzwOqYZTVxRYEIABZyQJJoKcRFUwW+e5Hm\nvE0DTNTPb0sfXkZ63Kf/ZECDPcHSD1hmIOe2CXbdAArobSEqzqC/rVLdNUTZalkd\n2xspEwbtzabhxQGNNYfMo8sZfw==\n-----END PRIVATE KEY-----\n",
+//   "client_email": "firebase-adminsdk-fgrrr@socialape-4e17c.iam.gserviceaccount.com",
+//   "client_id": "118420894873735894282",
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fgrrr%40socialape-4e17c.iam.gserviceaccount.com"
+// appId: "1:1092512616884:web:1df6c451777c50b63022e1",
+// measurementId: "G-TV94T73RE6"
+// }
+
+
+
+module.exports = {
+  apiKey: "AIzaSyBIKffOIRqs-CrOs8TP2iruDVRaTzughmY",
+  authDomain: "socialape-4e17c.firebaseapp.com",
+  databaseURL: "https://socialape-4e17c.firebaseio.com",
+  projectId: "socialape-4e17c",
+  storageBucket: "socialape-4e17c.appspot.com",
+  messagingSenderId: "1092512616884",
+};
+
+
